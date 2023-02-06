0a06b52de2f644d6b47a09ddffe0ba8e33ca72e6
niroshanmalidu <123802907+niroshanmalidu@users.noreply.github.com>
2023-02-06 18:51:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
-global.owner = ['17722386341'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
-global.mods = ['5219991402134'] //No tiene utilidad
-global.prems = ['5219996125657' , '5219992095479', '972557048356'] //Podra usar algunos comandos del apartado de propietario
+global.owner = ['94714184446'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
+global.mods = ['94714184446'] //No tiene utilidad
+global.prems = ['94714184446 , '94716682532', '94704119750'] //Podra usar algunos comandos del apartado de propietario
 
 //Lo siguiente no tiene nada que ver con el funcionamiento del Bot:
 //Nota: Puedes contactarme si necesitas ayuda con algo al +5219996125657 (este es mi numero personal/real bloqueo personas que no sean temas de la instalación)
@@ -55,12 +55,12 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = 'The Shadow Brokers - Bot'
+global.packname = '(☞ﾟ∀ﾟ)☞ NIROSHAN'
+global.author = 'The NIROSHAN-Shadow Brokers - Bot'
 
 
-global.wm = '©The Shadow Brokers - Bot'
-global.wait = '*Espere un momento..*'
+global.wm = '©The NIROSHAN Shadow Brokers - Bot'
+global.wait = '*wait a moment please ✨️✨️..*'
 
 
 global.multiplier = 69 // The higher, The harder levelup
